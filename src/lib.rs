5205ee6dcd7b7a1e38834c7ae8693cd8d0fce012
Andrew Hickman <andrew.hickman1@sky.com>
2025-10-11 13:25:35 +0100

    Add transform interpolation systems

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
+mod agent;
 
+use bevy::prelude::*;
+
+pub use self::agent::{Agent, Velocity};
+
+/// Plugin for adding [jostle](crate) functionality to an app.
+#[derive(Debug, Default)]
+pub struct JostlePlugin;
+
+/// System set for [jostle](crate) in the [FixedPostUpdate] schedule.
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
+pub struct JostleSystems;
+
+impl Plugin for JostlePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(FixedFirst, agent::update_physical_position);
+        app.add_systems(
+            RunFixedMainLoop,
+            agent::update_render_position.in_set(RunFixedMainLoopSystems::AfterFixedMainLoop),
+        );
+    }
+}
